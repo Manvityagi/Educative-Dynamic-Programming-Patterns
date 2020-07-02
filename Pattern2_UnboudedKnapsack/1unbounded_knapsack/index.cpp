c0b3d57d3d012f91fe9bda738896277584b452fe
Manvi Tyagi <manvityagi770@gmail.com>
2020-07-02 14:01:58 +0530

    changes

--- Pattern2_UnboudedKnapsack/1unbounded_knapsack/index.cpp
+++ Pattern2_UnboudedKnapsack/1unbounded_knapsack/index.cpp
@@ -1,23 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define PB push_back
-#define F first
-#define S second
-#define MP make_pair
-#define LL long long
-#define ULL unsigned long long
-#define LB lower_bound
-#define MOD1 1000000007
-#define MOD2 1000000009
-#define INF LONG_MAX
-#define pr(i,x,n) for(int ii=i;ii<n+i;ii++){cout<<x[ii]<<" ";}cout<<endl;
-#define ip(i,x,n) for(int ii=i;ii<n+i;ii++){cin>>x[ii];};
-#define db(x,y) cout<<x<<" "<<y<<endl;
-#define N 400100
-#define LD long double
-#define PR pair<LD,LL>
-
 int unboundedKnapsack(int W, int n, int val[], int wt[]) 
 { 
     // dp[i] is going to store maximum value 
@@ -54,11 +36,3 @@ int main() {
     return 0;
 }
 
-
-
-static const int _ = []() {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  return 0;
-}();
--- Pattern2_UnboudedKnapsack/coin change/recursive.cpp
+++ Pattern2_UnboudedKnapsack/coin change/recursive.cpp
@@ -2,21 +2,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define PB push_back
-#define F first
-#define MP make_pair
-#define LL long long
-#define ULL unsigned long long
-#define LB lower_bound
-#define MOD1 1000000007
-#define MOD2 1000000009
-#define INF LONG_MAX
-#define pr(i,x,n) for(int ii=i;ii<n+i;ii++){cout<<x[ii]<<" ";}cout<<endl;
-#define ip(i,x,n) for(int ii=i;ii<n+i;ii++){cin>>x[ii];};
-#define db(x,y) cout<<x<<" "<<y<<endl;
-#define N 400100
-#define LD long double
-#define PR pair<LD,LL>
 
 int coin(vector<int> coins,int n,int total) {
     //base case
